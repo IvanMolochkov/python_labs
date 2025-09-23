1c5295b165459042285a671add33efca411813f7
Joker_1050_ti <130921210+IvanMolochkov@users.noreply.github.com>
2025-09-23 13:32:15 +0300

    первая лаба(fix2)

--- lab1/5.py
+++ lab1/5.py
@@ -1,9 +1,12 @@
-fio = input().strip()
+fio = input()
 arr = []
+new_fio = ""
 for e in fio.split():
     arr.append(e[0])
+    new_fio += f"{e} "
 ini = "".join(arr)
-print(f"ФИО: {fio}")
+new_fio = new_fio.strip()
+print(f"ФИО: {new_fio}")
 print(f"Инициалы: {ini}")
-print(f"Длина (символов): {len(fio)}")
+print(f"Длина (символов): {len(new_fio)}")
 
