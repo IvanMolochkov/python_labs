0079e89126133f4c800e5536908bdec161ed25da
Joker_1050_ti <130921210+IvanMolochkov@users.noreply.github.com>
2025-09-29 11:01:30 +0300

    вторая лаба(fix5)

--- lab2/tuples.py
+++ lab2/tuples.py
@@ -2,11 +2,10 @@ def format_record(kor):
     if len(kor[0]) == 0: return "ValueError"
     if len(kor[1]) == 0: return "ValueError"
     if type(kor[2]) != float: return "TypeError"
-    if 1 <= kor[2] <= 5: return "ValueError"
     res = f", гр. {kor[1]}, GPA {kor[2]:.2f}"
     a = kor[0].split()
     if len(a) == 3: res = f"{a[0][0].upper()}{a[0][1:]} {a[1][0].upper()}. {a[2][0].upper()}.".strip() + res
-    elif len(a) == 2: res = f"{a[0][0]}. {a[1][0]}." + res
+    elif len(a) == 2: res = f"{a[0][0].upper()}{a[0][1:]}. {a[1][0].upper()}." + res
     else: return "ValueError"
     return res
 
